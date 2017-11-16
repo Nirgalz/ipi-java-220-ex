5b2d9cd36ddffebd0e5af5d2bab8b4ca615359ee
Nirgalz <znirgal@gmail.com>
2017-11-16 11:55:47 +0100

    fgh

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -5,7 +5,30 @@ import org.joda.time.LocalDate;
 public  class Commercial extends Employe{
 
     private Double caAnnuel;
-    Integer performance;
+    private  Integer performance;
+    private enum notes{
+        INSUFFISANT, PASSABLE, BIEN, TRES_BIEN
+    }
+
+//    public static notes equivalenceNote()
+//    {
+//        switch (this.performance){
+//            case 0 :
+//                return notes.INSUFFISANT;
+//        }
+//    }
+
+
+    public boolean performanceEgale(Integer perf)
+    {
+        if (performance.equals(perf)) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     @Override
     public Double getPrimeAnnuelle() {
@@ -13,7 +36,7 @@ public  class Commercial extends Employe{
         Double prime;
         if (caAnnuel == null || caAnnuel == 0) caAnnuel = 0.0;
 
-         prime = Math.ceil(caAnnuel * 0.05);
+        prime = Math.ceil(caAnnuel * 0.05);
 
         if (prime<500) prime = 500.0;
 
@@ -40,6 +63,12 @@ public  class Commercial extends Employe{
     public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
         super(nom, prenom, matricule, dateEmbauche, salaire);
         this.caAnnuel = caAnnuel;
+    }
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel, Integer performance) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+        this.performance = performance;
     }
 
     public Commercial(Double caAnnuel) {
